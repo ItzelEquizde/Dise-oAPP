2fd3d3264d967b1780b55d29a3461283545d1387
ItzelEquizde <86081169+ItzelEquizde@users.noreply.github.com>
2022-12-08 11:38:41 -0600

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ProyectoFacheritoXD',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
